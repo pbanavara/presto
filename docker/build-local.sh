7cb45f93372ec09e1877cc8102d6225589b83728
Pradeep Banavara <pradeepbs@gmail.com>
2020-11-17 11:28:08 +0530

    Adding changes to Dockerfile and build-local.sh for Aerospike connect jars

--- docker/build-local.sh
+++ docker/build-local.sh
@@ -12,18 +12,21 @@ PRESTO_VERSION=$(./mvnw --quiet help:evaluate -Dexpression=project.version -Dfor
 popd
 
 WORK_DIR="$(mktemp -d)"
+mkdir ${WORK_DIR}/aero
 cp ../presto-server/target/presto-server-${PRESTO_VERSION}.tar.gz ${WORK_DIR}
 tar -C ${WORK_DIR} -xzf ${WORK_DIR}/presto-server-${PRESTO_VERSION}.tar.gz
 rm ${WORK_DIR}/presto-server-${PRESTO_VERSION}.tar.gz
 cp -R bin default ${WORK_DIR}/presto-server-${PRESTO_VERSION}
 
 cp ../presto-cli/target/presto-cli-${PRESTO_VERSION}-executable.jar ${WORK_DIR}
+cp ../plugin/presto-aerospike-0.3.0/*.jar ${WORK_DIR}/aero/
+
 
 docker build ${WORK_DIR} --pull -f Dockerfile --build-arg "PRESTO_VERSION=${PRESTO_VERSION}" -t "presto:${PRESTO_VERSION}"
 
 rm -r ${WORK_DIR}
 
 # Source common testing functions
-. container-test.sh
+#. container-test.sh
 
-test_container "presto:${PRESTO_VERSION}"
+#test_container "presto:${PRESTO_VERSION}"
